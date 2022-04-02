b1dcef9f565ad58cbb0f4a0aad8eaf8efc9f6f3a
Thallys Henrique <thallys10@gmail.com>
2022-04-02 10:23:18 +0100

    Commit exercicio 7, calculo de conta telefonica

---
+++ ExerciseBeginner/Exer7_conta_tel/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        Locale.setDefault(Locale.US);
+
+        /*Entrar com o tempo em min*/
+        System.out.println("Quantos minutos foram utilizados:");
+
+        /*Receber os minutos na variavel criada*/
+        int minutes = sc.nextInt();
+        double billValue = 50;
+        if (minutes > 100) {
+            billValue = billValue + (minutes - 100)*2;
+        }
+        else {
+            billValue = billValue;
+        }
+
+        /*Mostrar o valor final da conta*/
+        System.out.printf("Você usou %d minutos e o valor da conta é de R$ %.2f. %n Obrigado!", minutes, billValue);
+       sc.close();
+    }
+}
